b4fcfb94820794ab1054159728e7f5f7a5318e73
ant1x1x <antoniokoussa7@gmail.com>
2024-06-19 16:10:36 +0100

    initial commit

--- src/pages/home/home.components.jsx
+++ src/pages/home/home.components.jsx
@@ -11,7 +11,7 @@ import Drinks  from "../../assetts/images/drinks.jpg";
 import Bakery from "../../assetts/images/bakery.jpg";
 import Donut from "../../assetts/images/donut.jpg";
 import Coffee from "../../assetts/images/coffee.jpg";
-import Toy from "../../assetts/images/toy.jpg";
+// import Toy from "../../assetts/images/toy.jpg";
 import Care from "../../assetts/images/care.jpg";
 import Clean  from "../../assetts/images/cleaning.jpg";
 import Man  from "../../assetts/images/1.webp";
@@ -35,7 +35,11 @@ import Man17  from "../../assetts/images/18.webp";
 import Bd  from "../../assetts/images/bd.jpg";
 import fng from "../../assetts/images/fng.jpg";
 import mp from "../../assetts/images/mp.jpg";
-import ToyImage from '../../assetts/images/toy.jpg';
+import reci from "../../assetts/images/reci.png"
+import wl from "../../assetts/images/wl.jpg"
+import baby from "../../assetts/images/baby.png"
+import ToyImage from "../../assetts/images/toy.jpg"
+import Liquor from '../../assetts/images/liquor.jpg';
 import ShopriteImage from "../../assetts/images/shoprittee.png";
 import "./home.styles.css";
 
@@ -113,7 +117,7 @@ const HomePage= ()=>{
             </div>
 
             <div>
-                <img src={Toy} alt="#" className="imag"/>
+                <img src={Liquor} alt="#" className="imag"/>
                 <p className="pea">Liquor</p>
             </div>
             
@@ -123,7 +127,7 @@ const HomePage= ()=>{
             </div>
 
             <div>
-                <img srcn={Clean} alt="#" className="imag"/>
+                <img src={Clean} alt="#" className="imag"/>
                 <p className="pea">Cleaning</p>
             </div>
 
@@ -150,92 +154,92 @@ const HomePage= ()=>{
             <Link to="">
             <img src={Man} alt="#" className="image"/>
             </Link>
-            <p>Cape Leopard Leap Lookout White Wine 750Ml</p>
+            <p className="apel">Cape Leopard Leap Lookout White Wine 750Ml</p>
            </div>
 
            <div>
             <img src={Tan1} alt="#" className="image"/>
-            <p>Apples (Red) X 12</p>
+            <p className="apel">Apples (Red) X 12</p>
            </div>
 
            <div>
             <img src={Ban2} alt="#" className="image"/>
-            <p>Fox’s Chunkie White Chocolate Cookies 180g</p>
+            <p className="apel">Fox’s Chunkie White Chocolate Cookies 180g</p>
            </div>
             
            <div>
             <img src={Van3} alt="#" className="image"/>
-            <p>Funtuna Prepacked Eggs X30</p>
+            <p className="apel">Funtuna Prepacked Eggs X30</p>
            </div>
 
            <div>
             <img src={Can4} alt="#" className="image"/>
-            <p>Grape Red Seedless</p>
+            <p className="apel">Grape Red Seedless</p>
            </div>
 
            <div>
             <img src={Aan5} alt="#" className="image"/>
-            <p>Chips Pringles 165G, Original</p>
+            <p className="apel">Chips Pringles 165G, Original</p>
            </div>
 
            <div>
             <img src={Yan6} alt="#" className="image"/>
-            <p>Peak Full Cream Milk Powder 360g Refill</p>
+            <p className="apel">Peak Full Cream Milk Powder 360g Refill</p>
            </div>
 
            <div>
             <img src={Man7} alt="#" className="image"/>
-            <p>Apples Green: Golden Delicious X6</p> 
+            <p className="apel">Apples Green: Golden Delicious X6</p> 
            </div>
 
            <div>
             <img src={Man8} alt="#" className="image"/>
-            <p>Tampax Super Plus Tampons 20S Pack</p>
+            <p className="apel">Tampax Super Plus Tampons 20S Pack</p>
            </div>
 
            <div>
             <img src={Man9} alt="#" className="image"/>
-            <p>Dano Powder Milk 350g</p>
+            <p className="apel">Dano Powder Milk 350g</p>
            </div>
 
            <div>
             <img src={Man10} alt="#" className="image"/>
-            <p>Titus Frozen Fish Gsf 1Kg</p>
+            <p className="apel">Titus Frozen Fish Gsf 1Kg</p>
            </div>
 
            <div>
             <img src={Man11} alt="#" className="image"/>
-            <p>Grape Red Globe PP 1S Pack</p>
+            <p className="apel">Grape Red Globe PP 1S Pack</p>
            </div>
 
            <div>
             <img src={Man12} alt="#" className="image"/>
-            <p>Pringles 165G, Texas Bbq</p>
+            <p className="apel">Pringles 165G, Texas Bbq</p>
            </div>
 
            <div>
             <img src={Man13} alt="#" className="image"/>
-            <p>Lemon Imported X6</p>
+            <p className="apel">Lemon Imported X6</p>
            </div>
 
            <div>
             <img src={Man14} alt="#" className="image"/>
-            <p>Frosty Bite Ice Cream 550Ml Strawberry</p>
+            <p className="apel">Frosty Bite Ice Cream 550Ml Strawberry</p>
            </div>
 
            <div>
             <img src={Man15} alt="#" className="image"/>
-            <p>Funtuna Prepacked Eggs X24</p>
+            <p className="apel">Funtuna Prepacked Eggs X24</p>
            </div>
 
            <div>
             <img src={Man16} alt="#" className="image"/>
-            <p>Energy Drink Regular Red Bull 6X250ml Can</p>
+            <p className="apel">Energy Drink Regular Red Bull 6X250ml Can</p>
            </div>
 
            <div>
             <img src={Man17} alt="#" className="image"/>
-            <p>Apple Royal Gala PP 1S Pack</p>
+            <p className="apel">Apple Royal Gala PP 1S Pack</p>
            </div>
         </div>
     </div>
@@ -266,8 +270,45 @@ const HomePage= ()=>{
                 <p className="header3">Delicious as a roast with crisp crackling,<br></br> irresistible as sticky finger-licking ribs <br></br> dripping with mouth-watering flavour, or <br></br>flash fried and served in a fragrant stir-fry, <br></br>pork really is the most versatile…</p>
                  <button className="bott">Read more</button>
             </div>
+            <div className="division2">
+                <img src={reci} className="imas" alt="#"/>
+                <h1 className="spanish3">Recipes</h1>
+                <p className="header3">A recipe is a formula of ingredients and a list of instructions for creating prepared foods. It is used to control quality, quantity, and food costs in a foodservice operation. </p>
+                 <button className="bott">Read more</button>
+            </div>
+            <div className="division2">
+                <img src={wl} className="imas" alt="#"/>
+                <h1 className="spanish3">Wine & Liquor</h1>
+                <p className="header3">At Shoprite, we’ve made sure to stock the widest range of your favourite drinks, all at supermarket prices. From truly exclusive whiskies to the world’s top wines, it’s never been more…</p>
+                 <button className="bott">Read more</button>
+            </div>
+            <div className="division2">
+                <img src={baby} className="imas" alt="#"/>
+                <h1 className="spanish3">Baby</h1>
+                <p className="header3">Welcome to our Baby Hub! From pregnancy to your baby’s first steps, we’ve got all the baby tips you need to know for your child’s development. We’ll be your guide as expecting parents through…</p>
+                 <button className="bott">Read more</button>
+            </div>
         </div>
-          
+    </section>
+    <section className="sector3">
+        <div className="division3">
+        <h1 className="header4">
+        Stay in the know!
+        </h1>
+        <span className="spanish6">Sign up today to receive up-to-date information about promotions and discounts.</span>
+        </div>
+        <div className="division5">
+        <form action="#" className="form2">
+        <input className="input-form" type="text" placeholder="Full Name" />
+        </form>
+        <form action="#" className="form2">
+        <input className="input-form" type="email" placeholder="Email Address" />
+        </form>
+        </div>
+        <div>
+            <h1>3 + 8 =</h1>
+            
+        </div>
     </section>
         </>
     )
